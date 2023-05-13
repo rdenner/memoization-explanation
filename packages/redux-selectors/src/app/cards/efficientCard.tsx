933ee0c08b29430732705af4d700566706c5f635
Ruben Denner <ruben-francois.denner@hpe.com>
2023-05-13 11:35:01 +0200

    Add notes

--- packages/redux-selectors/src/app/cards/efficientCard.tsx
+++ packages/redux-selectors/src/app/cards/efficientCard.tsx
@@ -5,18 +5,29 @@ import { isEqual, map, reduce } from 'lodash'
 import { RootState } from '../../store'
 
 const EfficientCard: FC = () => {
-  const counters: Record<string, boolean> = useSelector(
-    (state: RootState) =>
-      reduce(
-        state.counter.counters,
-        (prev, curr, key) => ({
-          ...prev,
-          [key]: curr > 0,
-        }),
-        {}
-      ),
-    isEqual
-  )
+  // Create a record of all counters with the values indicating if they are
+  // positive or not.
+
+  const counters: Record<string, boolean> = useSelector((state: RootState) => {
+    // This runs on every Redux state update to see if there is a change in the
+    // result.
+    return reduce(
+      state.counter.counters,
+      (prev, curr, key) => ({
+        ...prev,
+        [key]: curr > 0,
+      }),
+      {}
+    )
+  }, isEqual) // We need to use deep equality since the memory reference of the object is always new
+
+  // Problems with this approach?
+  // - Deep equality can be expensive
+  // - We still do calculations on every state update
+
+  // Positives?
+  // - Simple
+
   return (
     <Card>
       <CardHeader title="Efficient" />
--- packages/redux-selectors/src/app/cards/inefficientCard.tsx
+++ packages/redux-selectors/src/app/cards/inefficientCard.tsx
@@ -5,6 +5,28 @@ import { map, reduce } from 'lodash'
 import { RootState } from '../../store'
 
 const InefficientCard: FC = () => {
+  // Create a record of all counters with the values indicating if they are
+  // positive or not.
+
+  // Not scalable to have multiple selectors:
+  // const isPositive1 = useSelector(
+  //   (state: RootState) => get(state.counter.counters, 'counter-1', 0) > 0
+  // )
+  // const isPositive2 = useSelector(
+  //   (state: RootState) => get(state.counter.counters, 'counter-2', 0) > 0
+  // )
+
+  // Can't conditionally use hooks:
+  // const counters: Record<string, boolean> = {}
+  // forEach(['counter-1', 'counter-2'], (key) => {
+  //   // React Hook "useSelector" cannot be called inside a callback
+  //   counters[key] = useSelector(
+  //     (state: RootState) => get(state.counter.counters, key, 0) > 0
+  //   )
+  // })
+
+  // Need to do processing inside selector.
+  // Why is this also bad? See efficientCard.
   const counters: Record<string, boolean> = useSelector((state: RootState) =>
     reduce(
       state.counter.counters,
@@ -15,6 +37,7 @@ const InefficientCard: FC = () => {
       {}
     )
   )
+
   return (
     <Card>
       <CardHeader title="Inefficient" />
